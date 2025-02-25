f6af34c180ab972219aa522944123815c4bea835
Nguyen Le Tan Thanh <nltt.work6677@gmail.com>
2025-02-25 17:01:36 +0700

    Create 404 page

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    props: {
+        modelValue: {
+            type: String,
+            default: ''
+        },
+    },
+    emits: ['submit', 'update:modelValue'],
+    method: {
+        updateModelValue(e) {
+            this.$emit('update:modelValue', e.target.value);
+        },
+    },
+    submit(){
+        this.$emit('submit');
+    },
+};
+</script>
+
+<template>
+    <div class="'input-group">
+        <input type="text"
+        class='form-control'
+        placeholder="Nhập thông cần tìm"
+        vlaue="modelValue"
+        @input="updateModelValue"
+        @keyup.enter="submit"
+        >
+        <div class="input-group-append">
+            <button class="btn btn-outline" type="button" @click="submit">
+                <i class="fa fa-search">Tìm kiếm<main></main></i>
+            </button>
+        </div>
+    </div>
+    
+</template>
